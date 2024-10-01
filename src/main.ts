f9f01343b0da8b17d7373351d5c56da41274d8ea
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-10-01 11:02:36 +0500

    users activation_link not finished yet

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser"
 // import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function start() {
   try {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
     // app.useGlobalPipes(new CustomValidationPipe());
     const config = new DocumentBuilder()
