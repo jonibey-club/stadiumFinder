5ebb9577c79c2334a643174f659f57a3514e85d8
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-10-02 09:49:54 +0500

    bot on method added

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,11 +1,21 @@
-import { Ctx, Start, Update } from "nestjs-telegraf";
+import { Ctx, On, Start, Update } from "nestjs-telegraf";
 import { Context } from "telegraf";
 
 
 @Update()
-export class BotUpdate{
+export class BotUpdate {
   @Start()
-  async onStart(@Ctx() ctx: Context){
-    await ctx.reply("Bot started")
+  async onStart(@Ctx() ctx: Context) {
+    await ctx.reply("Bot started");
+  }
+
+  @On("text")
+  async onText(@Ctx() ctx: Context) {
+    console.log(ctx);
+    if ("text" in ctx.message) {
+      if (ctx.message.text == "salom")
+        await ctx.replyWithHTML("<b> Hello </b>");
+      else await ctx.replyWithHTML(ctx.message.text);
+    }
   }
 }
